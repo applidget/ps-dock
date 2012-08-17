64f08ee263c5bc4fb2238ffc7c97f396dd88f034
Svyatoslav Covanov <svyatoslav.covanov@applidget.com>
2012-08-17 18:19:12 +0200

    copied log-rotate branch

--- ps-dock.js
+++ ps-dock.js
@@ -1,9 +1,9 @@
 #!/usr/bin/env node
-
 var runner = require('./lib/child_process_runner')
 , logging = require('./lib/logging')
 , optionsHandler = require('./lib/handle_options.js')
 , notificatorLib = require('./lib/notify_api.js')
+, distantSocket = require('./lib/distant_socket.js')
 , util = require('util');
 
 
@@ -21,12 +21,51 @@ childProcess.updateEvent(function(status){
   notificator.notifyApi(status);
 });
 
+var buffer = '';
+var bufferize = function(data){
+  buffer += data;
+}
+childProcess.output.on('data', bufferize);
+var sock;
+if(optionsHandler.options.distantSocket != undefined){
+  sock = distantSocket.createDistantSocket(optionsHandler.options);
+  sock.on('end', function(){
+    childProcess.kill('SIGINT', 2)
+    socketOpened = false;
+  });
+  sock.on('connect', function(){
+    var socketOpened = true;
+    childProcess.output.removeListener('data',bufferize);
+    sock.write(buffer);
+    buffer = ''
+    childProcess.output.on('data', function(data){
+      if(socketOpened){
+        sock.write('' + data);
+      }
+    });
+  });
+  setTimeout(function(){
+    if(!socketOpened){
+      sock.write('test')
+      sock.close(function(){ process.exit(returnCode) });
+    }
+  }, 30000);
+  sock.on('data', function(data){
+    childProcess.input.write(data);
+  });
+}
 childProcess.on('end', function(returnCode){
   notificator.on('end', function(){
-    process.exit(returnCode);
+    if(optionsHandler.options.distantSocket != undefined){
+      sock.close(function(){
+        process.exit(returnCode);
+      });
+    }
+    else{
+      process.exit(returnCode);
+    }
   });
 });
-
 var signals = { 'SIGINT': 2, 'SIGTERM': 15, 'SIGHUP': 1, 'SIGKILL': 9, 'SIGPIPE': 13, 'SIGALRM': 14, 'SIGQUIT': 15};
 
 for (var signal in signals){
