2f2325583c2e6579d8963d8f69dd55a45f18033b
Svyatoslav Covanov <svyatoslav.covanov@applidget.com>
2012-08-22 14:46:29 +0200

    made class tcp_logging

--- ps-dock.js
+++ ps-dock.js
@@ -3,7 +3,7 @@ var runner = require('./lib/child_process_runner')
 , logging = require('./lib/logging')
 , optionsHandler = require('./lib/handle_options')
 , notificatorLib = require('./lib/notify_api')
-, distantSocket = require('./lib/distant_socket')
+, tcpLogging = require('./lib/tcp_logging')
 , util = require('util');
 var psDock = this;
 
@@ -16,35 +16,31 @@ var signals = { 'SIGINT': 2, 'SIGTERM': 15, 'SIGHUP': 1, 'SIGKILL': 9, 'SIGPIPE'
 
 var optionsHandler = optionsHandler.createHandler(realArgs, function(optionsHandler){
   logger = logging.createFileLogger(optionsHandler.options);
+  logger = tcpLogging.createTcpLogger(optionsHandler.options);
   notificator = notificatorLib.createNotificator(optionsHandler.options.webHookUrl, optionsHandler.options.timeout);
+//  childProcess = runner.runChildProcess(optionsHandler.options, logger);
   childProcess = runner.runChildProcess(optionsHandler.options, logger);
   childProcess.updateEvent(function(status){
     util.log('Updating API status');
     notificator.notifyApi(status);
   });
   var exit = function (returnCode){
-    if(optionsHandler.options.distantSocket != undefined){
-      sock.close(function(){
-        process.exit(returnCode);
-      });
-    }
-    else{
+    logger.close(function(){
       process.exit(returnCode);
-    }
+    });
   }
   childProcess.on('end', function(returnCode){
     notificator.on('end', function(){
-      if(logger.isChangingLogs){
-        logger.on('logRotateFinished', function(){
+      if(logger.loggerIsNotAvailable){
+        logger.on('loggerIsAvailable', function(){
           exit(returnCode);
         });
       }
       else {
-          exit(returnCode);
+        exit(returnCode);
       }
     });
   });
-  var sock = distantSocket.createDistantSocket(optionsHandler.options, childProcess.stream);
   for (var signal in signals){
     process.on(signal, childProcess.killChildProc(signals[signal], signal));
   }
